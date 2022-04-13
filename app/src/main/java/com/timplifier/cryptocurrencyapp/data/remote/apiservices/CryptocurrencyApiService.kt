7fcc201a0717179b39abaf447ed95a339beb1421
Clar1fy <iamonmydoctorpath@gmail.com>
2022-04-13 17:05:59 +0600

    add function to fetch a signle cryptocurrency

--- app/src/main/java/com/timplifier/cryptocurrencyapp/data/remote/apiservices/CryptocurrencyApiService.kt
+++ app/src/main/java/com/timplifier/cryptocurrencyapp/data/remote/apiservices/CryptocurrencyApiService.kt
@@ -2,8 +2,12 @@ package com.timplifier.cryptocurrencyapp.data.remote.apiservices
 
 import com.timplifier.cryptocurrencyapp.data.remote.dtos.CryptocurrencyDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface CryptocurrencyApiService {
     @GET("coins")
     suspend fun fetchCryptocurrency(): List<CryptocurrencyDto>
+
+    @GET("coins/{coin_id}")
+    suspend fun fetchSingleCryptocurrency(@Path("coin_id") id: Int): CryptocurrencyDto
 }
