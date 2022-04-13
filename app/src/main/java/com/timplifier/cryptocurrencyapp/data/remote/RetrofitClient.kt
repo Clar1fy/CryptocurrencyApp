3053070967e836595c60572440759ccda31bc40a
Clar1fy <iamonmydoctorpath@gmail.com>
2022-04-13 16:58:12 +0600

    add retrofitclient and baseurl in constants

--- app/src/main/java/com/timplifier/cryptocurrencyapp/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/timplifier/cryptocurrencyapp/data/remote/RetrofitClient.kt
@@ -1,4 +1,29 @@
 package com.timplifier.cryptocurrencyapp.data.remote
 
+import com.timplifier.cryptocurrencyapp.common.constants.Constants.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 class RetrofitClient {
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(provideLoggingInterceptor())
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .build()
+
+    private fun provideLoggingInterceptor() =
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttpClient)
+        .build()
+
+
 }
