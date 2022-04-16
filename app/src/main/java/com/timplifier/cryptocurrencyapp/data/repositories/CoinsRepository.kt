1cc3026f8b37e024dff4c2730348be88a42c353c
Clar1fy <iamonmydoctorpath@gmail.com>
2022-04-16 13:20:59 +0600

    refactor coins repository

--- app/src/main/java/com/timplifier/cryptocurrencyapp/data/repositories/CoinsRepository.kt
+++ app/src/main/java/com/timplifier/cryptocurrencyapp/data/repositories/CoinsRepository.kt
@@ -1,19 +1,28 @@
 package com.timplifier.cryptocurrencyapp.data.repositories
 
 import com.timplifier.cryptocurrencyapp.base.BaseRepository
+import com.timplifier.cryptocurrencyapp.data.local.room.daos.CoinDao
 import com.timplifier.cryptocurrencyapp.data.remote.apiservices.CryptocurrencyApiService
 import javax.inject.Inject
 
 class CoinsRepository @Inject constructor(
-    private val cryptocurrencyApiService: CryptocurrencyApiService
+    private val cryptocurrencyApiService: CryptocurrencyApiService,
+    private val coinDao: CoinDao
 ) : BaseRepository() {
 
-    fun fetchCoins() = sendRequest {
-        cryptocurrencyApiService.fetchCryptocurrency()
-    }
+    fun fetchCoins() = sendRequest(
+        { cryptocurrencyApiService.fetchCryptocurrency() },
+        { coins -> coinDao.insertCoin(*coins.toTypedArray()) }
+    )
+
 
     fun fetchSingleCoin(id: String) = sendRequest {
         cryptocurrencyApiService.fetchSingleCryptocurrency(id)
     }
+
+    fun getCoins() = sendRequest {
+        coinDao.getCoins()
+    }
+
 
 }
