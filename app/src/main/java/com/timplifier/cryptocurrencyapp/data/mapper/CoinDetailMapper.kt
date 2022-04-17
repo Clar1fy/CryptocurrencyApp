263dea622b9c3496140e0aaed41d933668de66f4
Clar1fy <iamonmydoctorpath@gmail.com>
2022-04-17 14:14:07 +0600

    finish coindetailmapper

--- app/src/main/java/com/timplifier/cryptocurrencyapp/data/mapper/CoinDetailMapper.kt
+++ app/src/main/java/com/timplifier/cryptocurrencyapp/data/mapper/CoinDetailMapper.kt
@@ -5,6 +5,62 @@ import com.timplifier.cryptocurrencyapp.domain.model.CoinDetail
 
 
 fun CoinDetailEntity.toCoinDetail() = CoinDetail(
+    name = name,
+    symbol = symbol,
+    parent = parent,
+    rank = rank,
+    isNew = isNew,
+    isActive = isActive,
+    tags = tags,
+    type = type,
+    teamEntity = teamEntity,
+    description = description,
+    message = message,
+    openSource = openSource,
+    hardwareWallet = hardwareWallet,
+    startedAt = startedAt,
+    developmentStatus = developmentStatus,
+    proofType = proofType,
+    orgStructure = orgStructure,
+    hashAlgorithm = hashAlgorithm,
+    contract = contract,
+    platform = platform,
+    contracts = contracts,
+    links = links,
+    linksExtended = linksExtended,
+    whitepaper = whitepaper,
+    firstDataAt = firstDataAt,
+    lastDataAt = lastDataAt
 
+
+)
+
+fun CoinDetail.toCoinEntity() = CoinDetailEntity(
+    name = name,
+    symbol = symbol,
+    parent = parent,
+    rank = rank,
+    isNew = isNew,
+    isActive = isActive,
+    tags = tags,
+    type = type,
+    teamEntity = teamEntity,
+    description = description,
+    message = message,
+    openSource = openSource,
+    hardwareWallet = hardwareWallet,
+    startedAt = startedAt,
+    developmentStatus = developmentStatus,
+    proofType = proofType,
+    orgStructure = orgStructure,
+    hashAlgorithm = hashAlgorithm,
+    contract = contract,
+    platform = platform,
+    contracts = contracts,
+    links = links,
+    linksExtended = linksExtended,
+    whitepaper = whitepaper,
+    firstDataAt = firstDataAt,
+    lastDataAt = lastDataAt
 
 )
